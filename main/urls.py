1b075d2f5671807f686dd3144dbb6f290d9ae8a7
Rai Freire <rai.freire26@gmail.com>
2024-01-12 17:12:40 -0300

    v_02
    versao 02 do projeto

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.alunoView, name='alunos_list'),
+    path('alunoID/<int:id>',views.alunoIDview, name='aluno-detalhe'),
+    path('contato',views.contactView, name='contato'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Aluno
-from django.http import HttpResponse
 
 def alunoView(request):
     alunos_list = Aluno.objects.all
     # return HttpResponse('exibindo os alunos')
     return render(request, 'main/alunos.html',{'alunos_list':alunos_list})
+
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk=id)
+    print(aluno)
+    return render(request,'main/alunoID.html',{'aluno': aluno})    
+
+
+def contactView(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print('Nome: '+name, ' Email: '+email, 'Mensagem: '+message)
+    return render(request, 'main/contact.html')
